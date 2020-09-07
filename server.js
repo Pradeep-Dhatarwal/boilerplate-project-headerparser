b5662581562be9b33d9b8c8c690e4960741431e6
Pradeep Singh <pradsngh523@gmail.com>
2020-09-07 20:52:17 +0000

    parsed json headers

--- server.js
+++ server.js
@@ -20,8 +20,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+let data = JSON.parse(JSON.stringify(req.headers));
+  res.json({
+    "ipaddress": data["x-forwarded-for"],
+    "language": data["accept-language"],
+    "software": data["user-agent"]
+  });
 });
 
 
